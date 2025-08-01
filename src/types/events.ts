00ba3171242d1fb6c8c4622d673878b1a89e0f70
akshatsabavat <akshat.sabavat@gmail.com>
2025-08-02 03:27:49 +0530

    Defined: Sales Event Message

--- src/types/events.ts
+++ src/types/events.ts
@@ -8,3 +8,20 @@ export interface InventoryMessage {
   store_id: number;
   inv_update: InventoryUpdate[];
 }
+
+export interface SalesEvent {
+  purchases: {
+    product_id: number;
+    product_current_stock: number;
+    product_name: string;
+    product_price: number;
+    productQuantity: number;
+  }[];
+  purchase_time: string;
+}
+
+export interface SalesMessage {
+  store_id: number;
+  user_id: string;
+  sales_records: SalesEvent[];
+}
