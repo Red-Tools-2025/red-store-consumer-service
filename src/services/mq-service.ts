ef7800bfc7a8fa64bcb0d0c930006918bfb77c14
akshatsabavat <akshat.sabavat@gmail.com>
2025-08-01 10:02:08 +0530

    Simple MQ service script defined

--- src/services/mq-service.ts
+++ src/services/mq-service.ts
@@ -3,6 +3,16 @@ import { Consumer } from "kafkajs";
 // Script to run workers for cache + db updates
 export const mqService = async (consumerRegistery: Set<Consumer>) => {
   try {
+    // Running Each consumer from registery & Mapping Jobs to relevant topic messages
+    for (const consumer of consumerRegistery) {
+      await consumer.connect();
+      await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          console.log(`📩 ${topic} - ${message} [${partition}]`);
+        },
+      });
+    }
+    console.log("MQ Consumers started successfully 😀");
   } catch (err) {
     console.log(`Error during MQ service run ❌🥲`, err);
   }
